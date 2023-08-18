ab6d01bd4f9451c157eba3bc9ad979a7aa8eda6d
Alpha Coma <400829150120@r3-1.deped.gov.ph>
2023-08-18 11:42:28 +0800

    chore: upgrade package dependencies

--- src/components/pages/index/body/home/index.tsx
+++ src/components/pages/index/body/home/index.tsx
@@ -1,37 +1,53 @@
 import GenButton from "@/components/gen/button";
+import GenImage from "@/components/gen/image";
 import styles from "./index.module.css";
 const Home: React.FC = () => {
 	return (
 		<div id="home" className={styles.home}>
-			<h1 className={styles.quote}>
-				Connecting minds <br />
-				Building Futures
-			</h1>
-			<p>
-				FEU Tech ACM Student Chapter is an organization operating exclusively
-				for educational and scientific purposes in order to promote to increased
-				knowledge and greater interest in science, design, development,
-				construction, languages, management and applications; and as a means of
-				communication between persons having an interest in computing.
-			</p>
-			<GenButton
-				props={{
-					label: "Become a Member",
-					onClick: () => console.log("Become a Member"),
-					className: `btnPurple`,
-				}}
-			>
-				Become a Member
-			</GenButton>
-			<GenButton
-				props={{
-					label: "Learn More",
-					onClick: () => console.log("Learn More"),
-					className: `btnWhite`,
-				}}
-			>
-				Learn More
-			</GenButton>
+			<div className={styles.homeContent}>
+				<h1 className={styles.quote}>
+					Connecting minds <br />
+					Building Futures
+				</h1>
+				<p>
+					FEU Tech ACM Student Chapter is an organization operating exclusively
+					for educational and scientific purposes in order to promote to
+					increased knowledge and greater interest in science, design,
+					development, construction, languages, management and applications; and
+					as a means of communication between persons having an interest in
+					computing.
+				</p>
+				<div className={styles.homeBtn}>
+					<GenButton
+						props={{
+							label: "Become a Member",
+							onClick: () => console.log("Become a Member"),
+							className: `btnPurple`,
+						}}
+					>
+						Become a Member
+					</GenButton>
+					<GenButton
+						props={{
+							label: "Learn More",
+							onClick: () => console.log("Learn More"),
+							className: `btnWhite`,
+						}}
+					>
+						Learn More
+					</GenButton>
+				</div>
+			</div>
+			<div className={styles.homeImg}>
+				<GenImage
+					props={{
+						src: "/branding/home-asset.png",
+						height: 400,
+						width: 400,
+						alt: "home",
+					}}
+				></GenImage>
+			</div>
 		</div>
 	);
 };
